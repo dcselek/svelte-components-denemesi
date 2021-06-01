744ab033c986f1c980ad6bea39f88e21737c3300
Doğan Can Selek <canselek2434@gmail.com>
2021-06-01 22:29:35 +0300

    button eklendi

---
+++ src/components/button.svelte
@@ -0,0 +1,17 @@
+<script>
+
+    export let color1 = false;
+    export let text;
+
+</script>
+
+{#if color1 == true }
+    <button class="pink" on:click={() => text = "Clicked"}>{text}</button>    
+{/if}
+
+
+<style>
+    .pink{
+        background-color: pink;
+    }
+</style>
